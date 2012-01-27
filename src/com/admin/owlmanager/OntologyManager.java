061aa6e007a61c8e116c1c2272526702c98ef936
Israel Fermin Montilla <iferminm@gmail.com>
2012-01-27 13:52:50 -0430

    added the first skelleton approach for the OntologyManager class

--- src/com/admin/owlmanager/OntologyManager.java
+++ src/com/admin/owlmanager/OntologyManager.java
@@ -1,6 +1,31 @@
 package com.admin.owlmanager;
 
-public class OntologyManager {
 
+/**
+ * This class manages all that has to do with the ontology
+ * 
+ * @author israelord
+ */
+public class OntologyManager {
+	
+	public void addTriplet() {
+		
+	}
+
+	public void deleteTriplet() {
+		
+	}
+	
+	public void deleteOntology() {
+		
+	}
+	
+	public void loadOntology() {
+		
+	}
+	
+	public void getTriplets() {
+		
+	}
 	
 }
