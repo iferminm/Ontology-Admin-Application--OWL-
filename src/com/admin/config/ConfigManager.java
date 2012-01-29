d6b2eb4a710eb4842df03a9dbb08388152be7536
Israel Fermin Montilla <iferminm@gmail.com>
2012-01-29 11:43:53 -0430

    wrote the constructor method to generate thr properties file or load it if it already exists

--- src/com/admin/config/ConfigManager.java
+++ src/com/admin/config/ConfigManager.java
@@ -1,12 +1,72 @@
 package com.admin.config;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * This class manages the properties file
  * 
  * @author israelord
  */
 public class ConfigManager {
-	
-	
 
+	private static ConfigManager instance = new ConfigManager();
+	private final String fileName = "ServerConfig.properties";
+	private Properties properties;
+	private String defaultValue = "not set";
+
+	
+	public static ConfigManager getInstance() {
+		return instance;
+	}
+	
+	private Properties generateProperties() {
+		properties = new Properties();
+		properties.setProperty("ModelsPath", defaultValue);
+		properties.setProperty("BaseModelName", defaultValue);
+		properties.setProperty("ReasonedModelName", defaultValue);
+		properties.setProperty("TestModelName", defaultValue);
+		properties.setProperty("VirtuosoConnection", defaultValue);
+		properties.setProperty("VirtuosoUser", defaultValue);
+		properties.setProperty("VirtuosoPassword", defaultValue);
+		properties.setProperty("GrapthName", defaultValue);
+		properties.setProperty("GraphLabel", defaultValue);
+		properties.setProperty("TestGraphName", defaultValue);
+		properties.setProperty("OntologyHistoryDirectory", defaultValue);
+		
+		return properties;
+	}
+	
+	private void generateFile(String name) {
+		FileOutputStream output;
+		try {
+			Properties properties = generateProperties();
+			output = new FileOutputStream(fileName);
+			properties.store(output, "Server Configuration File");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private ConfigManager() { 
+		try {
+			FileInputStream input = new FileInputStream(fileName);
+			properties.load(input);
+		} catch (FileNotFoundException e) {
+			new File(fileName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			generateFile(fileName);
+		}
+	}
 }
