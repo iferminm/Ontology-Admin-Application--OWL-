a5b98d82fe1fd48ada4b97c3310fb94c18298c66
Israel Fermin Montilla <iferminm@gmail.com>
2012-02-13 21:28:50 -0430

    added a domain package to put there all the abstraction classes *

---
+++ src/com/admin/domain/Triplet.java
@@ -0,0 +1,72 @@
+package com.admin.domain;
+
+/**
+ * This is an abstraction class to manage
+ * triplets as strings in an easier way
+ * 
+ * @author israelord
+ */
+public class Triplet {
+	
+	
+	
+	private Statement subject;
+	private Statement predicate;
+	private Statement object;
+	
+	public Triplet() { }
+	
+	public Triplet (String s, String p, String o) {
+		this.subject = new Statement(s);
+		this.predicate = new Statement(p);
+		this.object = new Statement(o);
+	}
+
+	/******************************************************************
+	 *                  GETTERS AND SETTERS                           * 
+	 ******************************************************************/
+	
+	public String getSubject() {
+		return subject.getStatement();
+	}
+
+	public void setSubject(String subject) {
+		this.subject.setStatement(subject);
+	}
+
+	public String getPredicate() {
+		return predicate.getStatement();
+	}
+
+	public void setPredicate(String predicate) {
+		this.predicate.setStatement(predicate);
+	}
+
+	public String getObject() {
+		return object.getStatement();
+	}
+
+	public void setObject(String object) {
+		this.object.setStatement(object);
+	}
+
+	
+	
+	/*************************************************************
+	 *        Simple getters, cleans the statements and          * 
+	 *         returns only the part of the string that          *
+	 *            matters to understand the statement            *
+	 *************************************************************/
+	
+	public String getSimpleSubject() {
+		return this.subject.getCleanStatement();
+	}
+	
+	public String getSimplePredicate() {
+		return this.predicate.getCleanStatement();
+	}
+
+	public String getSimpleObject() {
+		return this.object.getCleanStatement();
+	}
+}
