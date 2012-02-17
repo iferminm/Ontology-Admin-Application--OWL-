f4cda426e4c2f6c17948f915476ad542d23dc3d4
Israel Fermin Montilla <iferminm@gmail.com>
2012-02-17 15:34:03 -0430

    Added OntologManagerTest class

---
+++ src/com/admin/test/OntologyManagerTest.java
@@ -0,0 +1,26 @@
+package com.admin.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+
+import org.junit.Test;
+
+import com.admin.domain.Statement;
+import com.admin.owlmanager.OntologyManager;
+
+public class OntologyManagerTest {
+
+	@Test
+	public final void getClassesTest() {
+		OntologyManager om = new OntologyManager();
+		TreeSet<Statement> classes = om.getClasses();
+		
+		System.out.println(classes);
+		
+		assertTrue("ERROR:", classes.size() == 11);
+	}
+
+}
