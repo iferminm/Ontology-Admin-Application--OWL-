a5e9c7f78879d7f5cb4dac719bf2d487431e2bc7
Israel Fermin Montilla <iferminm@gmail.com>
2012-02-20 19:34:08 -0430

    added a test method for the one result query method

--- src/com/admin/test/OntologyManagerTest.java
+++ src/com/admin/test/OntologyManagerTest.java
@@ -17,9 +17,17 @@ public class OntologyManagerTest {
 		OntologyManager om = new OntologyManager();
 		TreeSet<Statement> classes = om.getClasses();
 		
-		System.out.println(classes);
-		
 		assertTrue("ERROR:", classes.size() == 11);
+	}
+	
+	@Test
+	public final void oneResultQueryTest() {
+		OntologyManager om = new OntologyManager();
+		String conditions = "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://localhost/ontologies/ThesisOntology.owl#KnowledgeArea> . " +
+							"?s <http://localhost/ontologies/ThesisOntology.owl#in-discipline> <http://localhost/ontologies/ThesisOntology.owl#ComputerScience>";
+		TreeSet<Statement> result = om.oneResultQuery("?s", conditions);
+		
+		assertTrue("Query Error", result.size() == 14);
 	}
 
 }
