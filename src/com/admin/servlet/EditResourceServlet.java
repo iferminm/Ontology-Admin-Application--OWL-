79919b8d98b34af116a37698d3a44b725a4ea7bc
Israel Fermin Montilla <iferminm@gmail.com>
2012-02-22 13:28:54 -0430

    wrote basic logic for resource editing and request filtering

--- src/com/admin/servlet/EditResourceServlet.java
+++ src/com/admin/servlet/EditResourceServlet.java
@@ -1,6 +1,10 @@
 package com.admin.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +30,36 @@ public class EditResourceServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		this.doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String action = request.getParameter("action");
+		String resource = request.getParameter("resource");
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		
+		if (action.equals("delete_resource")) {
+			writer.write("Borraremos el recurso: " + resource);
+		} else if (action.equals("delete_annotations")) {
+			writer.write("Borraremos las anotaciones: <br />");
+			Enumeration<String> paramNames = request.getParameterNames();
+			ArrayList<String> annotations = new ArrayList<String>();
+			
+			while (paramNames.hasMoreElements()) {
+				String current = paramNames.nextElement();
+				if (current.startsWith("http://")) {
+					writer.write(current + "<br />");
+					annotations.add(current);
+				}
+			}
+			writer.write("Del recurso: " + resource);
+		} else if (action.equals("add_annotations")) {
+			writer.write("agregaremos anotaciones al recurso: " + resource);
+		}
 	}
 
 }
